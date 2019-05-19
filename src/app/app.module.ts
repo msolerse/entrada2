d5d6d9879f46200e5ecfb0674bfc80ffaf08b504
Miquel Soler Serra <miquel@imac-de-miquel-soler-serra.local>
2019-05-19 19:10:05 +0200

    initial commit of full repository

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { TablaComponent } from './tabla/tabla.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TablaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
